a336aba0e988836b4e0b6e43ca20b4436a42c6cf
Daniel Moya <damr67@gmail.com>
2020-04-12 16:16:50 -0400

    add scss modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Button } from '@material-ui/core';
+import styles from './styles.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Button color="primary">Hello World</Button>
       </header>
     </div>
   );
