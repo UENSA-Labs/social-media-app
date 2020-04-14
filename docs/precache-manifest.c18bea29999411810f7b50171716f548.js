091a5cd5282eb31d3c8843e6fbfa7fa029483e4c
Daniel Moya <damr67@gmail.com>
2020-04-14 03:42:55 -0400

    Update Storybook

---
+++ docs/precache-manifest.c18bea29999411810f7b50171716f548.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7fe1995e4478d1462add4d97434ff8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.36c5ddc4f6d47d52330c.bundle.js"
+  },
+  {
+    "url": "runtime~main.36c5ddc4f6d47d52330c.bundle.js"
+  },
+  {
+    "url": "vendors~main.36c5ddc4f6d47d52330c.bundle.js"
+  },
+  {
+    "url": "vendors~main.36c5ddc4f6d47d52330c.bundle.js.LICENSE.txt"
+  }
+]);
