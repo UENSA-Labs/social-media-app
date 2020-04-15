342e16b77bd759b8c9e75994cc002f8d704f065b
Daniel Moya <damr67@gmail.com>
2020-04-15 07:38:35 -0400

    Add theme Provider (#11)
    * Add theme Provider
    
    * Add docs
    
    * add missing property

--- src/index.jsx
+++ src/index.jsx
@@ -1,43 +1,46 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
+import { initializeApp }  from 'firebase';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
+import ThemeProvider from './context/ThemeProvider';
 import App from './containers/App/index';
 import Home from './containers/Home/Home';
 import Profile from './containers/Profile/index';
 import './styles.scss';
 import { firebaseConfig } from './helpers/config';
 
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route
-          path="/es"
-          render={({ match }) => (
-            <App>
-              <Switch>
-                <Route exact path={`${match.url}`} component={Home} />
-                <Route exact path={`${match.url}/profile`} component={Home} />
-                <Route
-                  path={`${match.url}/profile/:username`}
-                  component={Profile}
-                />
-              </Switch>
-            </App>
-          )}
-        />
-      </Switch>
-      <Redirect to="/es" />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Switch>
+          <Route
+            path="/es"
+            render={({ match }) => (
+              <App>
+                <Switch>
+                  <Route exact path={`${match.url}`} component={Home} />
+                  <Route exact path={`${match.url}/profile`} component={Home} />
+                  <Route
+                    path={`${match.url}/profile/:username`}
+                    component={Profile}
+                  />
+                </Switch>
+              </App>
+            )}
+          />
+        </Switch>
+        <Redirect to="/es" />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
