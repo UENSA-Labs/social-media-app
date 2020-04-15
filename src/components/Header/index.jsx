8288cfa15d2effd2bc5efc5843a27e7b2fab9d25
Daniel Moya <damr67@gmail.com>
2020-04-15 07:22:33 -0400

    Add theme Provider

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
 // @flow
-import React from 'react';
+import React, { useContext }  from 'react';
+import { ThemeContext } from '../../context/ThemeProvider';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { 
+  AppBar,
+  Toolbar, 
+  Typography, 
+  Button, 
+  IconButton, 
+  Switch 
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
@@ -28,16 +32,23 @@ export type HeaderProps = {
 export default function Header({ login, logged }: HeaderProps) {
   const classes = useStyles();
 
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
-          </IconButton>
+          </IconButton> 
           <Typography variant="h6" className={classes.title}>
             Social Media App
           </Typography>
+          <Switch
+            color="secondary"
+            onChange={toggleTheme}
+            inputProps={{ 'aria-label': 'ThemeToggle' }}
+          />
           <Button color="inherit" onClick={login}>{ logged ? 'Welcome' : 'Login'}</Button>
         </Toolbar>
       </AppBar>
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,13 +1,16 @@
 // flow
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeProvider'; 
 import { Button } from '@material-ui/core';
 import logo from '../../logo.svg';
 import styles from './styles.module.scss';
 
 function Home() {
+  const { themeStyles } = useContext(ThemeContext);
+  
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
+      <header className={styles.appHeader} style={themeStyles}>
         <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
           Edit
